09dbe7a534d553e2f33b9248a359d4acd570b5b2
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-03 19:35:34 +0200

    Add Id property to HistoryItem

--- ChromeHistoryParser_ClassLib/HistoryItem.cs
+++ ChromeHistoryParser_ClassLib/HistoryItem.cs
@@ -2,13 +2,11 @@
 {
     public class HistoryItem
     {
+        public long Id { get; set; }
         public Uri URL { get; set; }
-
         public string Title { get; set; }
-
         public DateTime VisitedTime { get; set; }
         public long VisitCount { get; set; }
         public long TypedCount { get; set; }
-
     }
 }
