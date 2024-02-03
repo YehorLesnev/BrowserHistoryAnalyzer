74822eea077f0cfc4168a084a18ba2359858fa84
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-03 23:00:55 +0200

    Add Command implementation

---
+++ ChromeHistoryAnalyzer_WPF/ViewModels/Command.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChromeHistoryAnalyzer_WPF.ViewModels
+{
+    public class Command : ICommand
+    {
+        #region Constructor
+        public Command(Action<object> action)
+        {
+            ExecuteDelegate = action;
+        }
+        #endregion
+
+        #region Properties
+        public Predicate<object> CanExecuteDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+        #endregion
+
+        #region ICommand Members
+        public bool CanExecute(object? parameter)
+        {
+            if (CanExecuteDelegate != null)
+            {
+                return CanExecuteDelegate(parameter);
+            }
+
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (ExecuteDelegate != null)
+            {
+                ExecuteDelegate(parameter);
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #endregion
+    }
+}
