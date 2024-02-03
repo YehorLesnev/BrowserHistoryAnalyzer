32347ecee96365fd88878fce58e5c6388b7c4ea9
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-03 19:58:29 +0200

    Add ViewModels

---
+++ ChromeHistoryAnalyzer_WPF/ViewModels/ChromeHistoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChromeHistoryAnalyzer_WPF.ViewModels
+{
+    public class ChromeHistoryViewModel : ViewModelBase
+    {
+		private ObservableCollection<HistoryItemViewModel>? _historyItems;
+		public ObservableCollection<HistoryItemViewModel>? HistoryItems
+		{
+			get => _historyItems;
+            set
+			{
+                _historyItems = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
---
+++ ChromeHistoryAnalyzer_WPF/ViewModels/HistoryItemViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace ChromeHistoryAnalyzer_WPF.ViewModels
+{
+    public class HistoryItemViewModel : ViewModelBase
+    {
+        private long? _id;
+        public long? Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Uri? _url;
+        public Uri? Url
+        {
+            get => _url;
+            set
+            {
+                _url = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _title;
+        public string? Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime? _visitedTime;
+        public DateTime? VisitedTime
+        {
+            get => _visitedTime;
+            set
+            {
+                _visitedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private long? _visitCount;
+        public long? VisitCount
+        {
+            get => _visitCount;
+            set
+            {
+                _visitCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int? _typedCount;
+        public int? TypedCount
+        {
+            get => _typedCount;
+            set
+            {
+                _typedCount = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
