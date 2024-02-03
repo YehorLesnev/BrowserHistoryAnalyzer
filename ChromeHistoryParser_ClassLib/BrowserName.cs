a479ef61ceab7eeac1f11aec278a146dec02d3ce
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-03 22:52:10 +0200

    Update BrowserName.cs

--- ChromeHistoryParser_ClassLib/BrowserName.cs
+++ ChromeHistoryParser_ClassLib/BrowserName.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChromeHistoryParser_ClassLib
+﻿namespace ChromeHistoryParser_ClassLib
 {
     public enum BrowserName
     {
