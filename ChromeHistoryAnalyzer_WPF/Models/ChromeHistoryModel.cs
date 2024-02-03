944a6e3230658f8fdd06495be7a9500151d47bcc
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-03 19:30:53 +0200

    Add ChromeHistoryModel model class

---
+++ ChromeHistoryAnalyzer_WPF/Models/ChromeHistoryModel.cs
@@ -0,0 +1,16 @@
+﻿using ChromeHistoryParser_ClassLib;
+
+
+namespace ChromeHistoryAnalyzer_WPF.Models
+{
+    public class ChromeHistoryModel
+    {
+        public IEnumerable<HistoryItem>? HistoryItems { get; set; }
+
+        public ChromeHistoryModel() { }
+        public ChromeHistoryModel(IEnumerable<HistoryItem> historyItems)
+        {
+            this.HistoryItems = historyItems;
+        }
+    }
+}
