ef3720652a0fc425c694b571fe015545b79ca294
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-02-05 21:42:26 +0200

    Add exception handling

--- ChromeHistoryAnalyzer_WPF/ViewModels/BrowserHistoryViewModel.cs
+++ ChromeHistoryAnalyzer_WPF/ViewModels/BrowserHistoryViewModel.cs
@@ -1,33 +1,61 @@
-﻿using BrowserHistoryParser_ClassLib;
+﻿using BrowserHistoryAnalyzer_WPF.Views.Modals;
+using BrowserHistoryParser_ClassLib;
 using System.Collections.ObjectModel;
+using System.Data.SQLite;
 using System.Windows.Input;
 
 namespace BrowserHistoryAnalyzer_WPF.ViewModels
 {
     public class BrowserHistoryViewModel : ViewModelBase
     {
-		private readonly BrowserHistoryParser _parser = new();
+        private readonly BrowserHistoryParser _parser = new();
 
-		private ObservableCollection<HistoryItemViewModel>? _historyItems;
-		public ObservableCollection<HistoryItemViewModel>? HistoryItems
-		{
-			get => _historyItems;
+        private ObservableCollection<HistoryItemViewModel>? _historyItems;
+        public ObservableCollection<HistoryItemViewModel>? HistoryItems
+        {
+            get => _historyItems;
             set
-			{
+            {
                 _historyItems = value;
-				OnPropertyChanged();
-			}
-		}
+                OnPropertyChanged();
+            }
+        }
 
-		public BrowserHistoryViewModel()
-		{
-			GetAllHistory = new Command(getAllHistory);
-		}
+        public BrowserHistoryViewModel()
+        {
+            GetAllHistory = new Command(getAllHistory);
+            //ShowErrorModal = new Command(showErrorModal);
+        }
 
-		public ICommand GetAllHistory {get; set;}
-		private void getAllHistory(object o)
-		{
-            HistoryItems = _mapper.Map<ObservableCollection<HistoryItemViewModel>>(_parser.GetAllHistoryItems());
-		}
-	}
+        public ICommand GetAllHistory { get; set; }
+        private void getAllHistory(object o)
+        {
+            try
+            {
+                HistoryItems = _mapper.Map<ObservableCollection<HistoryItemViewModel>>(_parser.GetAllHistoryItems());
+            }
+            catch (SQLiteException e)
+            {
+                showErrorModal("\"" + e.Message + "\" Try to close all browsers and their processes");
+            }
+            catch (Exception e)
+            {
+                showErrorModal(e.Message);
+            }
+        }
+
+        //public ICommand ShowErrorModal { get; set; }
+        private void showErrorModal(object o)
+        {
+            var errWindow = new ErrorMessageWindow
+            {
+                ErrorMessage =
+                {
+                    Text = (string) o,
+                }
+            };
+
+            errWindow.ShowDialog();
+        }
+    }
 }
---
+++ ChromeHistoryAnalyzer_WPF/Views/Modals/ErrorMessageWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BrowserHistoryAnalyzer_WPF.Views.Modals
+{
+    /// <summary>
+    /// Interaction logic for ErrorMessageWindow.xaml
+    /// </summary>
+    public partial class ErrorMessageWindow : Window
+    {
+        public ErrorMessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
